63dc4686aff634c3afdc6edad52720459f2716c5
Matthew Bliffert <mattbliffert@icloud.com>
2018-01-22 12:43:53 -0500

    Wrapped fields object in function, creating proper closure, avoiding not defined errors on types, circular referencing fixed.

--- schema/schema.js
+++ schema/schema.js
@@ -6,15 +6,41 @@ const {
   GraphQLString,
   GraphQLInt,
   GraphQLSchema,
+  GraphQLList,
 } = graphql
+
+const ROOT_URL = 'http://localhost:3000'
+
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    users: {
+      type: new GraphQLList(UserType),
+      resolve(parentValue, args) {
+        return axios.get(`${ROOT_URL}/companies/${parentValue.id}/users`)
+          .then(response => response.data)
+      }
+    }
+  })
+})
 
 const UserType = new GraphQLObjectType({
   name: 'User',
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
-  },
+    company: {
+      type: CompanyType,
+      resolve(parentValue, args) {
+        return axios.get(`${ROOT_URL}/companies/${parentValue.companyId}`)
+          .then(response => response.data)
+      }
+    }
+  })
 })
 
 const RootQuery = new GraphQLObjectType({
@@ -24,10 +50,18 @@ const RootQuery = new GraphQLObjectType({
       type: UserType,
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
-        return axios.get(`http://localhost:3000/users/${args.id}`)
+        return axios.get(`${ROOT_URL}/users/${args.id}`)
           .then(response => response.data)
       },
     },
+    company: {
+      type: CompanyType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return axios.get(`${ROOT_URL}/companies/${args.id}`)
+          .then(response => response.data)
+      }
+    }
   },
 })
 
